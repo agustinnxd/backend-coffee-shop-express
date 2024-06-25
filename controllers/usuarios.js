e21389fb3e10433f6b8eca7546348002105dfcab
Agus <agusfer0525@gmail.com>
2024-06-25 17:08:48 -0300

    primer commit

---
+++ controllers/usuarios.js
@@ -0,0 +1,65 @@
+const { response, request } = require('express'); 
+
+const usuariosGet = (req = request, res = response) => {
+
+    const {q, nombre = 'no name', apikey, page = 1, limit} = req.query;
+
+    res.json({
+        ok: true,
+        msg: 'get API - controlador',
+        q,
+        nombre,
+        apikey,
+        page,
+        limit
+    })
+}
+
+const usuariosPut = (req = request, res = response) => {
+
+    const id = req.params.id
+
+    res.json({
+        ok: true,
+        msg: 'put API - controlador',
+        id
+    })
+}
+
+const usuariosPost = (req = request, res = response) => {
+
+    const { nombre, edad } = req.body;
+
+    res.status(201).json({
+        ok: true,
+        msg: 'post API - controlador',
+        nombre,
+        edad
+    })
+}
+
+const usuariosDelete = (req, res = response) => {
+    res.json({
+        ok: true,
+        msg: 'delete API - controlador'
+    })
+}
+
+const usuariosPatch = (req, res = response) => {
+    res.json({
+        ok: true,
+        msg: 'patch API - controlador'
+    })
+}
+
+
+
+
+
+module.exports = {
+    usuariosGet,
+    usuariosPut,
+    usuariosPost,
+    usuariosDelete,
+    usuariosPatch
+}
